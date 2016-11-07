df671438eae64e7e2d241cc59b4f1015b2b63cef
Maxym Bulgar <maksbulgar@gmail.com>
2016-11-07 13:49:33 +0200

    Use filter for index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.in_category(params[:category_id])
+    @products = Product.in_category_with_price_filter(params[:category_id],
+                                                  min_price: params[:min_price],
+                                                  max_price: params[:max_price])
   end
 
   def show
