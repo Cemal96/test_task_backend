0abbf62b8ba089da20dccc0d151b907ad9ad0e6c
Maxym Bulgar <maksbulgar@gmail.com>
2016-11-07 13:27:25 +0200

    Add query for product filter

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,9 @@ class Product < ApplicationRecord
   has_many :stores, through: :offers
   belongs_to :category
 
-  scope :in_category, ->(category_id) { includes(:stores).where(category_id: category_id) }
+  def self.in_category_with_price_filter(category_id, **param)
+    query = includes(:offers).where(category_id: category_id)
+    return query.where(offers: {price: param[:min_price]..param[:max_price]}) if param[:min_price] && param[:max_price]
+    return query
+  end
 end
